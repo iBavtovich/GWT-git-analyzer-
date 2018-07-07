34c2dd2d69658af09ce5e839d1e07e3642407660
Ignat Bavtovich <ignatbav@gmail.com>
2018-07-07 14:43:06 +0300

    Simple app + i18n added

--- src/main/java/com/epam/gwt/client/GithubAnalyzer.java
+++ src/main/java/com/epam/gwt/client/GithubAnalyzer.java
@@ -1,119 +1,43 @@
 package com.epam.gwt.client;
 
-import com.epam.gwt.shared.FieldVerifier;
+import com.epam.gwt.client.i18n.GitHubAnalyzerConstants;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class GithubAnalyzer implements EntryPoint {
-    private static final String SERVER_ERROR = "An error occurred while "
-            + "attempting to contact the server. Please check your network "
-            + "connection and try again.";
 
-    private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+    private final GitHubAnalyzerConstants constants = GWT.create(GitHubAnalyzerConstants.class);
 
     @Override
     public void onModuleLoad() {
-        final Button sendButton = new Button("Send");
         final TextBox nameField = new TextBox();
         nameField.setText("GWT User");
-        final Label errorLabel = new Label();
-
-        // We can add style names to widgets
+        final TextBox passwordField = new PasswordTextBox();
+        final Button sendButton = new Button(constants.loginButtonText());
         sendButton.addStyleName("sendButton");
 
-        // Add the nameField and sendButton to the RootPanel
-        // Use RootPanel.get() to get the entire body element
-        RootPanel.get("nameFieldContainer").add(nameField);
-        RootPanel.get("sendButtonContainer").add(sendButton);
-        RootPanel.get("errorLabelContainer").add(errorLabel);
+        VerticalPanel loginPanel = new VerticalPanel();
+        loginPanel.add(nameField);
+        loginPanel.add(passwordField);
+        loginPanel.add(sendButton);
 
-        // Focus the cursor on the name field when the app loads
-        nameField.setFocus(true);
-        nameField.selectAll();
-
-        // Create the popup dialog box
-        final DialogBox dialogBox = new DialogBox();
-        dialogBox.setText("Remote Procedure Call");
+        DialogBox dialogBox = new DialogBox();
+        dialogBox.setText(constants.dialogBoxTitle());
         dialogBox.setAnimationEnabled(true);
-        final Button closeButton = new Button("Close");
-        // We can set the id of a widget by accessing its Element
-        closeButton.getElement().setId("closeButton");
-        final Label textToServerLabel = new Label();
-        final HTML serverResponseLabel = new HTML();
-        VerticalPanel dialogVPanel = new VerticalPanel();
-        dialogVPanel.addStyleName("dialogVPanel");
-        dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-        dialogVPanel.add(textToServerLabel);
-        dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-        dialogVPanel.add(serverResponseLabel);
-        dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-        dialogVPanel.add(closeButton);
-        dialogBox.setWidget(dialogVPanel);
+        dialogBox.add(loginPanel);
 
-        // Add a handler to close the DialogBox
-        closeButton.addClickHandler(event -> {
-            dialogBox.hide();
-            sendButton.setEnabled(true);
-            sendButton.setFocus(true);
-        });
+        sendButton.addClickHandler(event -> Window.alert("Successfully logged in: " + nameField.getText()));
 
-        class MyHandler implements ClickHandler, KeyUpHandler {
-            public void onClick(ClickEvent event) {
-                sendNameToServer();
-            }
+        RootPanel.get().add(dialogBox);
 
-            public void onKeyUp(KeyUpEvent event) {
-                if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    sendNameToServer();
-                }
-            }
-
-            private void sendNameToServer() {
-                errorLabel.setText("");
-                String textToServer = nameField.getText();
-                if (!FieldVerifier.isValidName(textToServer)) {
-                    errorLabel.setText("Please enter at least four characters");
-                    return;
-                }
-
-                sendButton.setEnabled(false);
-                textToServerLabel.setText(textToServer);
-                serverResponseLabel.setText("");
-                greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-                    public void onFailure(Throwable caught) {
-                        dialogBox.setText("Remote Procedure Call - Failure");
-                        serverResponseLabel.addStyleName("serverResponseLabelError");
-                        serverResponseLabel.setHTML(SERVER_ERROR);
-                        dialogBox.center();
-                        closeButton.setFocus(true);
-                    }
-
-                    public void onSuccess(String result) {
-                        dialogBox.setText("Remote Procedure Call");
-                        serverResponseLabel.removeStyleName("serverResponseLabelError");
-                        serverResponseLabel.setHTML(result);
-                        dialogBox.center();
-                        closeButton.setFocus(true);
-                    }
-                });
-            }
-        }
-
-        MyHandler handler = new MyHandler();
-        sendButton.addClickHandler(handler);
-        nameField.addKeyUpHandler(handler);
+        dialogBox.show();
+        dialogBox.center();
     }
 }
---
+++ src/main/java/com/epam/gwt/client/i18n/GitHubAnalyzerConstants.java
@@ -0,0 +1,13 @@
+package com.epam.gwt.client.i18n;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface GitHubAnalyzerConstants extends Constants {
+
+    @DefaultStringValue("Login")
+    String loginButtonText();
+
+    @DefaultStringValue("Login form")
+    String dialogBoxTitle();
+
+}
